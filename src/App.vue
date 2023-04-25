3c8134f8b5bf63d9431ac036760ee7713df77293
xudan <875126243@qq.com>
2023-04-25 17:41:56 +0800

    fix  update

---
+++ src/App.vue
@@ -0,0 +1,141 @@
+<script setup>
+import * as THREE from 'three'
+import  Stats  from "three/examples/jsm/libs/stats.module"
+import  { Octree }  from "three/examples/jsm/math/Octree.js"
+import { Capsule } from "three/examples/jsm/math/Capsule.js"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import * as CANNON from "cannon-es"
+import { onMounted, reactive } from 'vue'
+
+onMounted (()=>{
+    // 默认场景
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x88ccee);
+    scene.fog = new THREE.Fog(0x88ccee, 0, 50);
+    // 场景1
+    const scene2 = new THREE.Scene();
+    // 场景2
+    const scene3 = new THREE.Scene();
+
+    // 相机
+    const camera = new THREE.PerspectiveCamera(
+      70,
+      window.innerWidth / window.innerHeight,
+      0.001,
+      1000
+    )
+    camera.position.set(0, 5, 10)
+
+    // 渲染器
+    const container = document.getElementById("container");
+    const renderer = new THREE.WebGL1Renderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.shadowMap.enabled = true;
+    renderer.shadowMap.type = THREE.VSMShadowMap;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    container.appendChild(renderer.domElement)
+
+    // 状态信息
+    const stats = new Stats();
+    stats.domElement.style.position = "absolute";
+    stats.domElement.style.top = '0px';
+    container.appendChild(stats.domElement);
+
+    // 轨道控制器
+    const controls = new OrbitControls(camera, renderer.domElement);
+    // controls.target.set(0, 0, 0);
+    
+
+    const clock = new THREE.Clock();
+    function animate() {
+      let delta = clock.getDelta();
+      stats.update();
+      controls.update();
+      renderer.render(scene, camera);
+      requestAnimationFrame(animate);
+    }
+    // 初始化物理世界
+    let world = new CANNON.World();
+    world.gravity.set(0, -9.82, 0)
+
+    // 默认弹性和摩擦系数
+    world.defaultContactMaterial.contactEquationStiffness = 1e9;
+    world.defaultContactMaterial.contactEquationRelaxation = 4;
+    // 迭代求解器，用于解决约束力学问题
+    const solver = new CANNON.GSSolver();
+    // 物理引擎在计算运动和碰撞时要执行的迭代步骤数
+    solver.iterations = 7;
+    // 物理引擎在计算过程中所允许的最大误差
+    solver.tolerance = 0.1;
+
+    world.solver = new CANNON.SplitSolver(solver)
+    
+    // 创建物理材质
+    const physicsMaterial = new CANNON.Material('physics');
+    const physics_physics = new CANNON.ContactMaterial(
+      physicsMaterial,
+      physicsMaterial,
+      {
+        friction: 0,
+        restitution: 0.3,
+      }
+    )
+    // 将物理材质添加到世界中
+    world.addContactMaterial(physics_physics);
+
+    // 创建一个球体
+    const radius = 1.3;
+    const sphereShape = new CANNON.Sphere(radius);
+    const sphereBody = new CANNON.Body({
+      mass: 5,
+      material: physicsMaterial,
+    })
+    sphereBody.addShape(sphereShape);
+    sphereBody.linearDamping = 0.9;
+
+    // 创建一个球体
+    const sphereGeometry = new THREE.SphereGeometry(radius, 8, 8);
+    const sphereMaterial = new THREE.MeshStandardMaterial({
+      color: 0xff0000,
+      wireframe: true
+    })
+    const sphereMesh = new THREE.Mesh(sphereGeometry,sphereMaterial)
+    scene.add(sphereMesh)
+    // 创建一个平面
+    const planeGeometry = new THREE.PlaneGeometry(20, 20, 1, 1);
+    const planMaterial = new THREE.MeshBasicMaterial({
+      color: 0xffffff,
+      side: THREE.DoubleSide
+    });
+    const plane = new THREE.Mesh(planeGeometry, planMaterial);
+    plane.receiveShadow = true;
+    plane.rotation.x = -Math.PI / 2;
+    scene.add(plane)
+
+    // // 创建胶囊几何体
+    // const capsuleGeometry  = new THREE.CapsuleGeometry(0.35, 1, 32);
+    // const capsuleMaterial = new THREE.MeshBasicMaterial({
+    //   color: 0xff0000,
+    //   side: THREE.DoubleSide
+    // });
+    // const capsule = new THREE.Mesh(capsuleGeometry, capsuleMaterial)
+    // capsule.position.set(0, 0.85, 0);
+    // capsule.castShadow = true;
+    // scene.add(capsule)
+
+    animate();
+});
+
+
+</script>
+
+<template>
+  <div id="container">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
